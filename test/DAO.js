d9b99a94ca77d58cab3586640763c6ee1f2aee76
harvey banning jr <hbanningjr@gmail.com>
2026-04-17 21:18:46 -0400

    Initial commit: DAO proposal creation with tests passing

---
+++ test/DAO.js
@@ -0,0 +1,130 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+const ether = tokens;
+
+describe("Token", () => {
+  let token, dao;
+  let deployer,
+    funder,
+    investor1,
+    investor2,
+    investor3,
+    investor4,
+    investor5,
+    recipient,
+    user;
+
+  beforeEach(async () => {
+    let accounts = await ethers.getSigners();
+    deployer = accounts[0];
+    funder = accounts[1];
+    investor1 = accounts[2];
+    investor2 = accounts[3];
+    investor3 = accounts[4];
+    investor4 = accounts[5];
+    investor5 = accounts[6];
+    recipient = accounts[7];
+    user = accounts[8];
+
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy("Dapp University", "DAPP", tokens(1000000));
+
+    //Send tokens to investors - each gets 20%
+    // Send tokens to investors - each gets 20%
+
+    let transaction;
+
+    transaction = await token.transfer(investor1.address, tokens(200000));
+    await transaction.wait();
+
+    transaction = await token.transfer(investor2.address, tokens(200000));
+    await transaction.wait();
+
+    transaction = await token.transfer(investor3.address, tokens(200000));
+    await transaction.wait();
+
+    transaction = await token.transfer(investor4.address, tokens(200000));
+    await transaction.wait();
+
+    transaction = await token.transfer(investor5.address, tokens(200000));
+    await transaction.wait();
+
+    //Deploy DAO
+    // Set quoroum to 50% of total supply + 1 token (to avoid ties)
+
+    const DAO = await ethers.getContractFactory("DAO");
+    dao = await DAO.deploy(token.address, "500000000000000000000001");
+
+    await funder.sendTransaction({ to: dao.address, value: ether(100) });
+
+    await token.transfer(investor1.address, tokens(100));
+  });
+
+  describe("Deployment", () => {
+    it("sends Ether to the DAO treasury", async () => {
+      expect(await ethers.provider.getBalance(dao.address)).to.equal(
+        ether(100),
+      );
+    });
+
+    it("returns token address", async () => {
+      expect(await dao.token()).to.equal(token.address);
+    });
+
+    it("returns quorom", async () => {
+      expect(await dao.quorum()).to.equal("500000000000000000000001");
+    });
+  });
+
+  describe("Proposal creation", () => {
+    let result;
+
+    describe("Success", () => {
+      beforeEach(async () => {
+        transaction = await dao
+          .connect(investor1)
+          .createProposal("Proposal 1", ether(1), recipient.address);
+        result = await transaction.wait();
+      });
+
+      it("updates proposal count", async () => {
+        expect(await dao.proposalCount()).to.equal(1);
+      });
+
+      it("updates proposal mapping", async () => {
+        const proposal = await dao.proposals(1);
+        console.log(proposal);
+        expect(proposal.id).to.equal(1);
+        expect(proposal.amount).to.equal(ether(1));
+        expect(proposal.recipient).to.equal(recipient.address);
+      });
+      it("emits a Propose event", async () => {
+        await expect(transaction)
+          .to.emit(dao, "Propose")
+          .withArgs(1, ether(1), recipient.address, investor1.address);
+      });
+    });
+
+    describe("Failure", () => {
+      it("rejects invalid amount", async () => {
+        await expect(
+          dao
+            .connect(investor1)
+            .createProposal("Proposal 1", ether(1000), recipient.address),
+        ).to.be.reverted;
+      });
+      it("rejects non-investor", async () => {
+        await expect(
+          dao
+            .connect(investor1)
+            .createProposal("Proposal 1", ether(100), recipient.address),
+        ).to.be.reverted;
+      });
+    });
+  });
+});
